4269f4c810d393dffbe0195a6c8dac5361fbdfca
jeromie <jeromie@ajency.in>
2017-07-06 17:02:43 +0530

    basic components added

---
+++ src/pages/modal/modal.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the ModalPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-modal',
+  templateUrl: 'modal.html',
+})
+export class ModalPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ModalPage');
+  }
+
+  dismiss(data) {
+    this.viewCtrl.dismiss(data);
+  }
+
+}
