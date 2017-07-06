3c869907ec8ca8906481ac595826983adf80ef4a
Antonio Furtado <antonio@ajency.in>
2017-07-06 18:07:08 +0530

    Enabled lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,15 +8,13 @@ import { IonicPage,NavController, NavParams, ModalController } from 'ionic-angul
 })
 export class HomePage {
 
-  private travel: string = "flights";
-
   constructor(public navCtrl: NavController,
   			  public navParams: NavParams,
   			  public modalCtrl: ModalController
   ) {
 
   }
-
+    
   showModal() {
       const modal = this.modalCtrl.create('ModalPage');
       modal.present();
